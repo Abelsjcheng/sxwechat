ca8ac2f57fa25a6099895278a63099621c12d5ed
Abelsjcheng <429433693@qq.com>
2019-08-05 17:25:19 +0800

    初始化用户服务模块页面

---
+++ src/components/home/village.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    美丽乡村页面待开发
+  </div>
+</template>
+<script>
+export default {
+  name: 'village', // 美丽乡村
+  components: {}, // 注册组件
+  data () { // 局内数据
+    return {
+    }
+  },
+  methods: { // 方法函数
+  },
+  computed: { // 计算属性
+
+  },
+  watch: { // 侦听器
+
+  },
+  mounted () { // 初始化函数
+
+  }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
