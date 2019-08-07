5d9c692269ad8c374318aae5dfd8e52095335c5a
Abelsjcheng <429433693@qq.com>
2019-08-07 09:43:06 +0800

    引入font-awesome

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 import store from './store/index.js'
-
+import 'font-awesome/less/font-awesome.less'
 Vue.config.productionTip = false
 // 移除移动端点击延迟
 const FastClick = require('fastclick')
