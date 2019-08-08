42e5e7921233044f327675235e0a8ff411ce9e66
Abelsjcheng <429433693@qq.com>
2019-08-08 15:09:44 +0800

    新建党建新闻页面

---
+++ src/components/party/partynews.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    党建新闻页面待开发
+  </div>
+</template>
+<script>
+export default {
+  name: 'partynews', // 党建新闻
+  components: {}, // 注册组件
+  data () { // 局内数据
+    return {
+    }
+  },
+  methods: { // 方法函数
+  },
+  computed: { // 计算属性
+
+  },
+  watch: { // 侦听器
+
+  },
+  mounted () { // 初始化函数
+
+  }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
