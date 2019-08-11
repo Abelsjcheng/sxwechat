1d48f824282375aff49afbf659b8b32615b82220
Abelsjcheng <429433693@qq.com>
2019-08-11 15:28:50 +0800

    修改格式错误

--- src/components/Infopanel/notice.vue
+++ src/components/Infopanel/notice.vue
@@ -1,8 +1,8 @@
 <template>
-   <div>
-	<panel :list="list" type="4"></panel>
+  <div>
+    <panel :list="list" type="4"></panel>
     <load-more tip="正在加载"></load-more>
-</div>
+  </div>
 </template>
 
 <script>
@@ -10,80 +10,78 @@
 import { Panel, LoadMore } from 'vux'
 
 export default {
-    name:'notice', //公告
-    components: {
-
-        Panel, LoadMore
-
-     },//注册组件
-    data() {//局内数据
-        return{ 
-            list: [
-                {
-                src: '',
-                title: '公告一',
-                desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道',
-                url: {
-                    path: '',
-                    replace: false
-                },
-                meta: {
-                    // source: '政策',
-                    other: '评论 20',
-                    date: '10分钟前'
-                }
-            },
-            {
-                src: '',
-                title: '公告二',
-                desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道',
-                url: {
-                    path: '',
-                    replace: false
-                },
-                meta: {
-                    // source: '公告',
-                    other: '评论 20',
-                    date: '10分钟前'
-                }},
-                {
-                src: '',
-                title: '公告三',
-                desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道',
-                url: {
-                    path: '',
-                    replace: false
-                },
-                meta: {
-                    // source: '政策',
-                    other: '评论 20',
-                    date: '10分钟前'
-                }
-                },
-                {
-                src: '',
-                title: '公告四',
-                desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道',
-                url: {
-                    path: '',
-                    replace: false
-                },
-                meta: {
-                    // source: '政策',
-                    other: '评论 20',
-                    date: '10分钟前'
-                }
-        }],
+  name: 'notice', // 公告
+  components: { Panel, LoadMore }, // 注册组件
+  data () { // 局内数据
+    return {
+      list: [
+        {
+          src: '',
+          title: '公告一',
+          desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道',
+          url: {
+            path: '',
+            replace: false
+          },
+          meta: {
+            // source: '政策',
+            other: '评论 20',
+            date: '10分钟前'
+          }
+        },
+        {
+          src: '',
+          title: '公告二',
+          desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道',
+          url: {
+            path: '',
+            replace: false
+          },
+          meta: {
+            // source: '公告',
+            other: '评论 20',
+            date: '10分钟前'
+          }
+        },
+        {
+          src: '',
+          title: '公告三',
+          desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道',
+          url: {
+            path: '',
+            replace: false
+          },
+          meta: {
+            // source: '政策',
+            other: '评论 20',
+            date: '10分钟前'
+          }
+        },
+        {
+          src: '',
+          title: '公告四',
+          desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道',
+          url: {
+            path: '',
+            replace: false
+          },
+          meta: {
+            // source: '政策',
+            other: '评论 20',
+            date: '10分钟前'
+          }
+        }
+      ]
       // footer: {
       //   title: '更多',
       //   url: ''
       // }
     }
-    },
-    methods: {//方法函数
+  },
+  methods: { // 方法函数
 
-    },
-     computed: { // 计算属性
+  },
+  computed: { // 计算属性
 
   },
   watch: { // 侦听器
