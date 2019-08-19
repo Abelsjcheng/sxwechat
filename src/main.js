140696f9d93d6d6ef46e1ca90602cadc1b4a9ba4
Abelsjcheng <429433693@qq.com>
2019-08-19 11:11:07 +0800

    引入axios

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import router from './router/index.js'
 import store from './store/index.js'
 import 'font-awesome/less/font-awesome.less'
 import './styles/common.less'
+
 Vue.config.productionTip = false
+const axios = require('axios')
+Vue.prototype.$axios = axios
 // 移除移动端点击延迟
 const FastClick = require('fastclick')
 FastClick.attach(document.body)
