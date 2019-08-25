0790e5e2cc54c822723412d2d3f3cdd9f129e744
WLS201741882203 <WLS201741882203@github.com>
2019-08-25 11:05:22 +0800

    消除警告

--- src/components/party/partywork.vue
+++ src/components/party/partywork.vue
@@ -51,24 +51,22 @@ export default {
   methods: { // 方法函数
     openguide (item) { // 显示弹窗
       this.showwork = true
-      
     },
     backpage () { // 关闭弹窗
       this.showwork = false
     },
-    onFocus (){
-        this.$refs.search.setBlur()
-        this.showtime=true
-        
+    onFocus () {
+      this.$refs.search.setBlur()
+      this.showtime = true
     },
-    selectdata (vag){
-      this.value=vag
+    selectdata (vag) {
+      this.value = vag
     },
-    getpartywork(){
-      this.$nextTick(() => { //等待html渲染结束
+    getpartywork () {
+      this.$nextTick(() => { // 等待html渲染结束
         console.log(this.data)
-        this.value=this.data
-      });
+        this.value = this.data
+      })
     }
   },
   computed: { // 计算属性
@@ -78,7 +76,7 @@ export default {
 
   },
   mounted () { // 初始化函数
-    this.getpartywork();
+    this.getpartywork()
   }
 }
 </script>
